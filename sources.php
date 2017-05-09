2a2c1e4dcaa7d12659e49d4575a4518ce2a6c6d2
cenoki <crisenoki@hotmail.com>
2017-05-09 14:00:11 -0400

    Add files via upload

--- sources.php
+++ sources.php
@@ -2,136 +2,81 @@
 include ("top.php");
 
 ?>
-    <article>
-        <h1>Sources</h1>
-        <h2>Articles</h2>
-        <p>https://www.britannica.com/</p>
-        <p>https://www.wikipedia.org/</p>
-        <p>https://www.lonelyplanet.com/</p>
-        <p>https://www.tripadvisor.com/</p>
-           
-        <h2>Images</h2><p>(in order of appearance)</p>
-        <h2>About us</h2>
-        <p>https://www.uvm.edu/</p>
-        <h2>Brazil</h2>
-        <p>http://www.getsready.com/rio-de-janeiro-awarded-destination/</p>
-        <p>https://i.ytimg.com/vi/hU8VqBZmsrI/maxresdefault.jpg</p>
-        <p>http://darkroom.baltimoresun.com/2014/03/march-4-daily-brief-mardi-gras-battle-of-the-oranges-and-ready-to-wear-fashions/topshots-brazil-carnival-rio-parade-unidos-da-tijuca-2/</p>
-        <p>https://en.wikipedia.org/wiki/Copacabana,_Rio_de_Janeiro</p>
-        <p>http://andrebaertschi.photoshelter.com/image/I0000T5KNDrh3U1E</p>
-        <p>http://www.procapacitar.com/2016/08/cursos-em-foz-do-iguacu.html</p>
-        <p>http://www.procapacitar.com/2016/08/cursos-em-foz-do-iguacu.html</p>
-        <p>http://www.ied.edu/ied-locations/sao-paulo</p>
-        <p>http://www.coringa.ind.br/index.php/calcule-sua-feijoada</p>
-        <p>https://www.natue.com.br/natuelife/wp-content/uploads/2014/03/churrasco2.jpg</p>
-        <p>http://www.guiadasemana.com.br/receitas/galeria/7-receitas-de-pao-de-queijo-saudavel-para-matar-a-vontade-de-comer</p>
-        <p>https://cuisinesamba.com/product/brigadeiro-gourmet/</p>
-        <p>http://www.craveonline.com/culture/981547-pride-brazil-cachaca-caipirinha</p>
-        
-        
-        <h2>Spain</h2>
-        <p>http://www.telegraph.co.uk/content/dam/Travel/Destinations/Europe/Spain/Madrid/madrid-overview-sunsetovermadrid-xlarge.jpg</p>
-        <p>https://d1bvpoagx8hqbg.cloudfront.net/originals/basilica-temple-expiatori-la-sagrada-familia-ae70d65f5731412b08b435ffef31819c.jpg</p>
-        <p>http://www.cntraveler.com/galleries/2016-03-29/the-most-beautiful-beaches-in-spain-and-portugal</p>
-        <p>http://barcelona-home.com/blog/guell/amazing-rooftop-view-from-parc-guell/</p>
-        <p>https://cdn.getyourguide.com/img/tour_img-482154-70.jpg</p>
-        <p>https://www.lonelyplanet.com/spain/mallorca</p>
-        <p>http://london.mandarinhouse.com/spanish-tapas-vs-cantonese-dim-sum/</p>
-        <p>http://www.elconfidencial.com/alma-corazon-vida/2016-01-27/como-hacer-la-paella-valenciana-perfecta_1141179/</p>
-        <p>http://www.alwaysorderdessert.com/2011/06/tortilla-espanola.html</p>
-        <p>https://www.tienda.com/products/sliced-jamon-iberico-peregrino-jm-23.html</p>
-        <p>https://www.buzzfeed.com/kirstenking/traditional-spanish-dishes-you-need-in-your-life?utm_term=.bxnjVGWb36#.uuzdG0we32</p>
-        
-        <h2>United Kingdom</h2>
-        <p>http://www.architecturaldigest.com/london-travel-guide</p>
-        <p>http://freephotooftheday.clientk.com/wp02/wp-content/uploads/2008/06/stonehenge-england.JPG</p>
-        <p>http://www.homesandproperty.co.uk/luxury/property/the-royal-property-portfolio-with-775-rooms-buckingham-palace-is-estimated-to-be-worth-2-billion-a100846.html</p>
-        <p>http://www.interopa.co.uk/The-visit-of-Tower-Bridge</p>
-        <p>http://www.dickins.co.uk/great-things-to-do-in-edinburgh/find-out-whats-on-in-edinburgh-this-june/</p>
-        <p>https://www.lonelyplanet.com/england/northwest-england/manchester</p>
-        <p>http://online-english-lessons.eu/wordpress/wp-content/uploads/2013/03/8578064794_11580cefae.jpg</p>
-        <p>http://citispotter.com/why-fish-and-chips-is-so-popular-in-england/</p>
-        <p>https://www.buzzfeed.com/jenniferschaffer/yum-british-food?utm_term=.eon627OzaY#.lgEO9vEqwL</p>
-        <p>http://www.seriouseats.com/2016/10/how-to-make-shepherds-pie-beef-lamb.html</p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
+   
+<?php
+
+// Sample code to open a plain text file
+
+$debug = false;
+
+if(isset($_GET["debug"])){
+    $debug = true;
+}
+
+$myFileName="data/sources";
+
+$fileExt=".csv";
+
+$filename = $myFileName . $fileExt;
+
+if ($debug) print "\n\n<p>filename is " . $filename;
+
+$file=fopen($filename, "r");
+
+// the variable $file will be empty or false if the file does not open
+if($file){
+    if($debug) print "<p>File Opened</p>\n";
+}
+
+// the variable $file will be empty or false if the file does not open
+if($file){
+    
+    if($debug) print "<p>Begin reading data into an array.</p>\n";
+
+    // the while (similar to a for loop) loop keeps executing until we reach 
+// the end of the file at which point it stops. the resulting variable 
+// $records is an array with all our data.
+
+    while(!feof($file)){
+        $sources[]=fgetcsv($file);
+    }
+    
+    //closes the file
+    fclose($file);
+    
+    if($debug) {
+        print "<p>Finished reading data. File closed.</p>\n";
+        print "<p>My data array<p><pre> "; print_r($records); print "</pre></p>";
+    }
+} // ends if file was opened
+?>
+
+<article class="sources">
+    <h1>Sources</h1>
+    <h2>Articles</h2>
+    
+    <?php
+    print '<ul>';
+    
+    foreach($sources as $source) {
+    
+    print "<li>". $source[0] . "</li>";
+    
+}
+print"</ul>"
+    
+    
+    ?>
+    
+    <h2>Images</h2>
+    <p>Courtesy of the Internet</p>
+    
+    
+    
+</article>
 
 
 
-    </article>
 <?php
 include ("footer.php");
 ?>   
